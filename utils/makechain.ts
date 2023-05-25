3ae16119e2ff9df7349c4e1ad089bc87faff9c8c
loxxo <water2ice@gmail.com>
2023-05-25 12:39:33 +0800

    更新langchain版本， verbose输出

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -1,6 +1,7 @@
 import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { ConversationalRetrievalQAChain } from 'langchain/chains';
+import { ConsoleCallbackHandler } from "langchain/callbacks";
 
 const CONDENSE_PROMPT = `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
 
@@ -12,22 +13,21 @@ Standalone question:`;
 const QA_PROMPT = `You are a helpful AI assistant. Use the following pieces of context to answer the question at the end.
 If you don't know the answer, just say you don't know. DO NOT try to make up an answer.
 If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
-You may be asked in different languages, answer the question using the same language.
 
 {context}
 
 Question: {question}
-Helpful answer in markdown:`;
+Helpful answer in markdown in Chinese:`;
 
 export const makeChain = (vectorstore: PineconeStore) => {
-  const model = new OpenAI({
+  const llm = new OpenAI({
     temperature: 0.1, // increase temepreature to get more creative answers
     modelName: 'gpt-3.5-turbo', //change this to gpt-4 if you have access
-    timeout: 5000, //ime
+    callbacks: [new ConsoleCallbackHandler()] 
   });
 
   const chain = ConversationalRetrievalQAChain.fromLLM(
-    model,
+    llm,
     vectorstore.asRetriever(),
     {
       qaTemplate: QA_PROMPT,
@@ -35,5 +35,7 @@ export const makeChain = (vectorstore: PineconeStore) => {
       returnSourceDocuments: true, //The number of source documents returned is 4 by default
     },
   );
+
+  chain.verbose = true
   return chain;
 };
